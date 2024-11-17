a92497d291b0a526927660301d3c28c41001dc4b
Kryptozek <youngsammy87@gmail.com>
2024-11-17 21:00:50 +0300

    Commit

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import permission_required
 from django.shortcuts import  redirect
+from .models import Book
 
 @permission_required('app_name.can_view', raise_exception=True)
 def view_instance(request, instance_id):
@@ -21,4 +22,13 @@ def edit_instance(request, instance_id):
 def delete_instance(request, instance_id):
     # Logic to delete an instance
     pass
+
+
+def book_list(request):
+    """
+    View to display a list of all books.
+    """
+    books = Book.objects.all()  # Fetch all books from the database
+    return render(request, 'bookshelf/book_list.html', {'books': books})
+
 
