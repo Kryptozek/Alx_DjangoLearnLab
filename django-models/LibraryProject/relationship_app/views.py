b75f7a5f8970764320baf43cdd5823a9db4571a2
Kryptozek <youngsammy87@gmail.com>
2024-11-17 17:57:14 +0300

    Commit

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -2,6 +2,10 @@ from django.shortcuts import render
 from django.views.generic.detail import DetailView
 from .models import Book# Import models from the current app
 from .models import Library
+from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import redirect
+from django.shortcuts import render
 
 # Function-based view to list all books
 def list_books(request):
@@ -13,4 +17,24 @@ class LibraryDetailView(DetailView):
     model = Library  # Specify the model to use for the view
     template_name = 'relationship_app/library_detail.html'  # Specify the template path
     context_object_name = 'library'  # Define the context object name for the template
+
+    # User login view (using Django's built-in LoginView)
+class CustomLoginView(LoginView):
+    template_name = 'relationship_app/login.html'
+
+# User logout view (using Django's built-in LogoutView)
+class CustomLogoutView(LogoutView):
+    template_name = 'relationship_app/logout.html'
+
+# User registration view (using Django's built-in UserCreationForm)
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')  # Redirect to login after successful registration
+    else:
+        form = UserCreationForm()
+    
+    return render(request, 'relationship_app/register.html', {'form': form})
 
