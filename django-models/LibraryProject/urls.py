d167dbbff2b7726e42bf207a587980533ae4740d
Kryptozek <youngsammy87@gmail.com>
2024-11-17 18:00:05 +0300

    Commit

--- django-models/LibraryProject/urls.py
+++ django-models/LibraryProject/urls.py
@@ -16,8 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('relationship_app/', include('relationship_app.urls')),
+    path('login/', views.CustomLoginView.as_view(), name='login'),
+    path('logout/', views.CustomLogoutView.as_view(), name='logout'),
+    path('register/', views.register, name='register'),
 ]
+
