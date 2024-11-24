3fc609a808c1149ee0d781e6b7b7497b1d3137d7
Kryptozek <youngsammy87@gmail.com>
2024-11-24 19:04:11 +0300

    Task 2 commit

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -2,14 +2,15 @@ from rest_framework.generics import ListAPIView  # Import ListAPIView
 from .models import Book
 from .serializers import BookSerializer
 from rest_framework.viewsets import ModelViewSet
+from rest_framework import viewsets  # Import ModelViewSet from DRF
 
 class BookList(ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
 
-class BookViewSet(ModelViewSet):
+class BookViewSet(viewsets.ModelViewSet):
     """
     A viewset for viewing and editing book instances.
     """
-    queryset = Book.objects.all()
-    serializer_class = BookSerializer
+    queryset = Book.objects.all()  # Query for fetching all books
+    serializer_class = BookSerializer  # Use the BookSerializer for serializing data
