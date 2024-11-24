8dcc741eba5273cad10bb3b261dc2af8dd92a9ce
Kryptozek <youngsammy87@gmail.com>
2024-11-24 18:43:14 +0300

    Task 1 Commit

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,9 +1,7 @@
-from django.shortcuts import render
-from rest_framework.generics import ListAPIView
+from rest_framework.generics import ListAPIView  # Import ListAPIView
 from .models import Book
 from .serializers import BookSerializer
 
 class BookList(ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
-
--- api_project/api_project/urls.py
+++ api_project/api_project/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from .views import BookList
+from api.views import BookList
 from django.contrib import admin
 from django.urls import include
 
