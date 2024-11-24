86f62395a2e5bc8dc0652dc97bb8fe892216df92
Kryptozek <youngsammy87@gmail.com>
2024-11-24 19:19:51 +0300

    Task 3 commit

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -3,6 +3,8 @@ from .views import BookList
 from rest_framework.routers import DefaultRouter
 from .views import BookViewSet  # Import BookList for the existing ListAPIView
 from django.urls import include
+from rest_framework.authtoken.views import obtain_auth_token
+from django.urls import path
 
 router = DefaultRouter()
 router.register(r'books_all', BookViewSet, basename='book_all')
@@ -15,8 +17,10 @@ urlpatterns = [
 
     # Include the router URLs for BookViewSet (all CRUD operations)
     path('', include(router.urls)),
-    
+
+    path('api-token-auth/', obtain_auth_token, name='api_token_auth'), 
 ]
+
 
 
 
--- api_project/api/views.py
+++ api_project/api/views.py
@@ -4,6 +4,8 @@ from .models import Book
 from .serializers import BookSerializer
 from rest_framework.viewsets import ModelViewSet
 from rest_framework import viewsets  # Import ModelViewSet from DRF
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
+from rest_framework.permissions import IsAdminUser
 
 class BookList(generics.ListAPIView):
     queryset = Book.objects.all()
@@ -15,3 +17,5 @@ class BookViewSet(viewsets.ModelViewSet):
     """
     queryset = Book.objects.all()  # Query for fetching all books
     serializer_class = BookSerializer  # Use the BookSerializer for serializing data
+    permission_classes = [IsAdminUser]
+    permission_classes = [IsAuthenticated]
