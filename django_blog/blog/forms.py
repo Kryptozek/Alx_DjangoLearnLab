d4c7b0706af64a414c76916f728dc1ef9e0268aa
Kryptozek <youngsammy87@gmail.com>
2024-12-08 22:53:42 +0300

    Commit

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -2,15 +2,13 @@ from django import forms
 from .models import Post
 from .models import Comment
 from .models import Tag
-from taggit.forms import TagField
+from taggit.forms import TagWidget 
 
 class PostForm(forms.ModelForm):
-    tags = forms.ModelMultipleChoiceField(
-            queryset=Tag.objects.all(),
-            widget=forms.CheckboxSelectMultiple,
-            required=False
-        )
-    tags = TagField(required=False)
+    tags = forms.CharField(
+        widget=TagWidget(),  # Use TagWidget for the tags field
+        required=False
+    )
     class Meta:
         widgets = {
             'title': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter the title'}),
